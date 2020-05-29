e094f6a1ba6428194622936ba3601b973bc90137
leo.song <403438485@qq.com>
2020-05-29 16:24:12 +0800

    [feature] add bison and flex into cmake

---
+++ src/parser/parser.h
@@ -0,0 +1,18 @@
+//
+// Created by songhaolin on 2020-05-29.
+//
+
+#ifndef SIMPLE_DATABASE_PARSER_H
+#define SIMPLE_DATABASE_PARSER_H
+
+namespace simpledb{
+
+class parser {
+
+};
+
+}//namespace simpledb
+
+
+
+#endif //SIMPLE_DATABASE_PARSER_H
